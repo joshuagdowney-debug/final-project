33e75a3eb93cad1ff19c29c951e0e79beb80c425
joshuagdowney-debug <joshuagdowney@gmail.com>
2026-04-06 17:45:24 -0400

    chanfe

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+main.py
+-------
+Controller that integrates all modules and launches the application.
+
+SYSC 2010 — Final Project
+Software System for Sensor-Based Data Acquisition,
+Signal Analysis, and Visualization
+
+Usage:
+    python main.py
+"""
+
+from gui import launch_gui
+
+
+def main():
+    launch_gui()
+
+
+if __name__ == "__main__":
+    main()
